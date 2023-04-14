2f9e64408e5345c508fb2f7bb66036a49e037f8d
Alexei Essine <alexei.essine@journeyone.com.au>
2023-04-14 17:16:26 +0800

    AE: Updated stack dynamo db tests.

--- lib/tests/stackTests/neptuneStack_dynamoDb.test.ts
+++ lib/tests/stackTests/neptuneStack_dynamoDb.test.ts
@@ -3,19 +3,43 @@ import {Template} from 'aws-cdk-lib/assertions';
 import {CustomStackProps} from '../../../bin/config';
 import {NeptuneWithVpcStack} from '../../neptuneStack';
 import {NEPTUNE_STACK_NAME} from '../../../bin/neptuneStreamMain';
-import { getCustomStackPropsTests } from './testHelper';
+import {getCustomStackPropsTests} from './testHelper';
 
 describe('Stack DynamoDb tests', () => {
   let stack: NeptuneWithVpcStack;
   let template: Template;
 
-
   const testStackProperties: CustomStackProps = getCustomStackPropsTests();
-  console.log(` STACK PROPS: ${JSON.stringify(testStackProperties)}`);
 
   beforeEach(() => {
     stack = new NeptuneWithVpcStack(new Stack(), NEPTUNE_STACK_NAME, testStackProperties);
     template = Template.fromStack(stack);
+  });
+
+  const tableConfigs = [
+    {name: testStackProperties.config.UPDATES_SUMMARY_TABLE, readCapacity: 1, writeCapacity: 1},
+    {name: testStackProperties.config.LATEST_PROCESSED_RECORD_TABLE, readCapacity: 1, writeCapacity: 1},
+    {name: testStackProperties.config.NEPTUNE_UPDATES_TABLE, readCapacity: 1, writeCapacity: 15},
+  ];
+
+  tableConfigs.forEach(({name, readCapacity, writeCapacity}) => {
+    it(`should create the ${name} table with proper properties`, () => {
+      template.hasResourceProperties('AWS::DynamoDB::Table', {
+        TableName: name,
+        PointInTimeRecoverySpecification: {
+          PointInTimeRecoveryEnabled: true,
+        },
+        ProvisionedThroughput: {
+          ReadCapacityUnits: readCapacity,
+          WriteCapacityUnits: writeCapacity,
+        },
+        Tags: [
+          {Key: 'CreatedBy', Value: 'CDK'},
+          {Key: 'Owner', Value: 'CDK'},
+          {Key: 'Purpose', Value: 'Neptune Cluster Stream demo'},
+        ],
+      });
+    });
   });
 
   it('should create the necessary tables', () => {
